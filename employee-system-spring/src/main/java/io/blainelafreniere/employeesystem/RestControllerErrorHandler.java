e7daba0745e0f0850aa8ebc1afad825c1716e23a
Blaine Lafreniere <brlafreniere@gmail.com>
2021-05-12 18:23:36 -0600

    - comment out rest controller error handler code

--- employee-system-spring/src/main/java/io/blainelafreniere/employeesystem/RestControllerErrorHandler.java
+++ employee-system-spring/src/main/java/io/blainelafreniere/employeesystem/RestControllerErrorHandler.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
 import java.util.HashMap;
 
+/*
 @RestControllerAdvice
 public class RestControllerErrorHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public HashMap<String, String> handleValidationException(ConstraintViolationException e) {
+    HashMap<String, String> handleValidationException(ConstraintViolationException e) {
         HashMap<String, String> errors = new HashMap<String, String>();
-        errors.put("foo", "bar");
+        errors.put("errors", e.getMessage());
         return errors;
     }
 }
+*/
