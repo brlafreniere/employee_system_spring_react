ed6e39a02f681d8a29fe9ea0bec625a2d53e9d68
Blaine Lafreniere <blaine@cmatm.com>
2021-05-19 10:31:02 -0600

    - add department to EmployeeType

--- react-frontend/src/types/EmployeeType.tsx
+++ react-frontend/src/types/EmployeeType.tsx
@@ -1,10 +1,12 @@
+import DepartmentType from "./DepartmentType"
+
 interface EmployeeType {
     id?: string,
     firstName: string,
     lastName: string,
     phoneNumber?: string,
     emailAddress: string,
-    departmentId: string
+    department: DepartmentType
 }
 
 export default EmployeeType
